bca94564a50cc4bea81338c3fa3ac6c01ec6e9dc
Mark <voronkov1mark@gmail.com>
2023-05-17 14:46:29 +0300

    initial commit

---
+++ src/main/java/com/pro/sky/hwlist/HwListApplication.java
@@ -0,0 +1,13 @@
+package com.pro.sky.hwlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pro/sky/hwlist/HwListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.hwlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
