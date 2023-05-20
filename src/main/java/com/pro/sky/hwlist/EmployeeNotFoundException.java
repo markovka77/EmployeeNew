d540d52fe83cbabff5a1a8b6ee370983af48bf82
Mark <voronkov1mark@gmail.com>
2023-05-20 03:00:50 +0300

    домашка

--- src/main/java/com/pro/sky/hwlist/EmployeeNotFoundException.java
+++ src/main/java/com/pro/sky/hwlist/EmployeeNotFoundException.java
@@ -1,8 +1,12 @@
 package com.pro.sky.hwlist;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeNotFoundException extends Exception{
     public EmployeeNotFoundException(String masssage) {
-        super(masssage);
+        super("EmployeeNotFound");
     }
 
 }
